71a97c1984dad28d0466a988321bbe28baa671c6
StanislavKls <73436574+StanislavKls@users.noreply.github.com>
2021-06-04 09:22:58 +0500

    for webhook

--- src/Bot.php
+++ src/Bot.php
@@ -4,5 +4,19 @@ namespace Leadsbot;
 
 class Bot
 {
-    //
+    private $token;
+    private $url;
+    private $client;
+
+    public function __construct($token)
+    {
+        $this->token  = $token;
+        $this->url    = "https://api.telegram.org/bot{$token}/";
+        $this->client = new \GuzzleHttp\Client(['base_uri' => $this->url]);
+    }
+    public function sendMessage($id, $text)
+    {
+        $params = ['chat_id' => $id,'text' => $text];
+        $response = $this->client->request('GET', 'sendMessage', ['query' => $params]);
+    }
 }
--- start.php
+++ start.php
@@ -2,4 +2,12 @@
 
 namespace Leadsbot;
 
-echo 'hello';
+require __DIR__ . './vendor/autoload.php';
+
+use Leadsbot\Bot;
+
+$token = "1844696370:AAE6l1RtoWWpSfdFi1luzKmpW3k-PaK-yG4";
+
+$bot = new Bot($token);
+
+$bot->sendMessage(149019377, 'test');
